e6f9b16ae3a98bc9607aa04f888b48cbb1fccb83
Christian Kabelin <81351453+ckabelin@users.noreply.github.com>
2022-04-25 09:30:38 +0200

    fix references to docker images

--- tdg-admin-ui/run-local.sh
+++ tdg-admin-ui/run-local.sh
@@ -10,9 +10,9 @@ export TDG_API_HOST_PORT=8080
 export TDG_API_KEY=SPEEDBOAT
 
 export TDG_IAM_SERVER_URL=http://192.168.1.224:9001/auth
-export TDG_IAM_RESOURCE=tdg-ui-secret
+export TDG_IAM_RESOURCE=tdg-ui
 export TDG_IAM_REALM=master
-export TDG_IAM_SECRET=FznOM51Y72LReGaCT4n2wqnUm21nnYXK
+export TDG_IAM_SECRET=xpWj3Eb8r7Hs9OZIFR8XAVqFpwuExQvC
 
 #mvn clean spring-boot:run -Dspring-boot.run.jvmArguments="-Xms2048m -Xmx4096m"
 mvn clean package
