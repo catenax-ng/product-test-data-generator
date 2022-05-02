e02e27c145843607d97c13b42a874f70f6f6bd14
Christian Kabelin <81351453+ckabelin@users.noreply.github.com>
2022-05-02 13:48:41 +0200

    local db backup enhancement

--- chart/testdatagenerator/db-template/export-zip.sh
+++ chart/testdatagenerator/db-template/export-zip.sh
@@ -2,9 +2,9 @@
 
 host="localhost"
 
-mongoexport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_data_template --out test_data_template.json
-mongoexport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_data_scenario --out test_data_scenario.json
-mongoexport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_meta_model --out test_meta_model.json
+mongoexport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_data_template --out test_data_template_local.json
+mongoexport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_data_scenario --out test_data_scenario_local.json
+mongoexport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_meta_model --out test_meta_model_local.json
 
 now=$(date +"%Y-%m-%d_%H_%M_%S")
 fname="archive-${now}.tar.gz"
--- chart/testdatagenerator/db-template/import.sh
+++ chart/testdatagenerator/db-template/import.sh
@@ -2,7 +2,7 @@
 
 prefix="mongoimport --authenticationDatabase 'admin'"
 
-mongoimport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_data_scenario --file /db-template/test_data_scenario.json 
-mongoimport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_data_template --file /db-template/test_data_template.json
-mongoimport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_meta_model    --file /db-template/test_meta_model.json
+mongoimport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_data_scenario --file /db-template/test_data_scenario_local.json 
+mongoimport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_data_template --file /db-template/test_data_template_local.json
+mongoimport -p "admin" -u "admin" --authenticationDatabase "admin" -d tdg --collection test_meta_model    --file /db-template/test_meta_model_local.json
 
